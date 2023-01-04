9ccd2233ea98324c013d06424bb3d5bfe79d8305
Nicolas Zamorano <na.zamovasquez@gmail.com>
2023-01-04 20:03:22 -0300

    se agrega funcionalidad al login, imagen agregada

--- pgy-proyect-tab/src/app/app-routing.module.ts
+++ pgy-proyect-tab/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'detalle-profesor',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
--- pgy-proyect-tab/src/app/pages/login/login.page.ts
+++ pgy-proyect-tab/src/app/pages/login/login.page.ts
@@ -8,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class LoginPage implements OnInit {
 
   constructor() { }
-
+  titulo_login = "Bienvenido a la aplicacion."
+  
   ngOnInit() {
+  }
+  user={
+    usuario:"ej nicolas"
   }
 
 }
