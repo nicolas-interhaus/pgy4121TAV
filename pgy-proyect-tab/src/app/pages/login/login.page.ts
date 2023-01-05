0f5c7f58131061b0a4a018dc8abce3ea59785928
Nicolas Zamorano <na.zamovasquez@gmail.com>
2023-01-04 21:15:29 -0300

    se valida login, boton desabilitado hasta cumplir caracteristicas requeridas

--- pgy-proyect-tab/src/app/pages/login/login.page.ts
+++ pgy-proyect-tab/src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+ import { ReactiveFormsModule,Validators,FormGroup,FormControl  } from '@angular/forms'; 
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,8 +14,18 @@ export class LoginPage implements OnInit {
   
   ngOnInit() {
   }
-  user={
-    usuario:"ej nicolas"
+   usuario = new FormGroup({
+    user: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]),
+    pass: new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)])
+  }) 
+  Validar_Alumno(){
+    if(this.usuario.value.user=="nicolas" && this.usuario.value.pass=="1234aaa"){
+      console.log("acceso concedido, bienvenido nicolas")
+      
+    }else{
+      console.log("acceso denegado, intentalo de nuevo")
+    }
   }
-
 }
+
+
